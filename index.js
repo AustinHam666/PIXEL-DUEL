4305209b06f711a6d02e01b77aecee4d41aa3149
Agustin <agusstingalvan2@gmail.com>
2025-06-17 19:41:01 -0300

    cosas en clase

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-import Menu from './scenes/menu.js';
-import Mapa1 from './scenes/mapa1.js';
-import Settings from './scenes/settings.js';
-import Credits from './scenes/credits.js';
+import Menu from "./scenes/menu.js";
+import Mapa1 from "./scenes/mapa1.js";
+import Settings from "./scenes/settings.js";
+import Credits from "./scenes/credits.js";
 
 const config = {
   type: Phaser.AUTO,
-  width: window.innerWidth,
-  height: window.innerHeight,
-  scene: [Menu, Mapa1, Settings, Credits],
+  width: 60 * 16,
+  height: 30 * 16,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    min: {
+      width: 400,
+      height: 225, // Mantiene la relación 16:9 (400 ÷ 16 × 9 = 225)
+    },
+    max: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    },
+  },
+  scene: [Mapa1, Settings, Credits],
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
-      debug: false
-    }
-  }
+      debug: false,
+    },
+  },
 };
 
 const game = new Phaser.Game(config);
--- scenes/mapa1.js
+++ scenes/mapa1.js
@@ -1,43 +1,64 @@
 export default class Mapa1 extends Phaser.Scene {
   constructor() {
-    super('mapa1');
+    super("mapa1");
   }
 
- init() {
-        this.totalItems = 0;
-    }
+  init() {
+    this.totalItems = 0;
+  }
 
   preload() {
-    this.load.image('tilemap', 'public/assets/objetos/texture.png');
-    this.load.image('naveazul', 'public/assets/objetos/naveazul.png');
-    this.load.image('naveroja', 'public/assets/objetos/naveroja.png');
-    this.load.tilemapTiledJSON('mapa1', 'public/assets/tilemap/mapateller1.json');
+    this.load.image("new-texture", "public/assets/objetos/new-texture.png");
+    //this.load.image("environment", "public/assets/objetos/environment.png");
+    this.load.tilemapTiledJSON("mapa1", "public/assets/tilemap/mapa1.json");
+    // this.load.image("naveazul", "public/assets/objetos/naveazul.png");
+    // this.load.image("pared", "public/assets/objetos/pared.png");
+    // this.load.image("fondo", "public/assets/objetos/fondo.png");
+    // this.load.image("naveroja", "public/assets/objetos/naveroja.png");
   }
 
-create() {
-  // Crea el fondo
-  const { width, height } = this.sys.game.config;
-  this.add.image(width/2, height/2, 'fondo').setDisplaySize(width, height);
+  create() {
+    // Crea el fondo
+    const { width, height } = this.sys.game.config;
 
-  // Crea el tilemap
-  const map = this.make.tilemap({ key: 'mapa1' });
-  const tileset = map.addTilesetImage('pared', 'tilemap'); 
-  const tileset2 = map.addTilesetImage('fondo', 'tilemap');
+    // Crea el tilemap
+    const map = this.make.tilemap({ key: "mapa1" });
+    const tileset = map.addTilesetImage("new-texture", "new-texture");
 
-  const plataformas = map.createLayer('plataforma', tileset, 0, 0);
-  plataformas.setCollisionByProperty({ collides: true }); // Si marcaste tiles como colisionables en Tiled
+    const fondo = map.createLayer("fondo", tileset, 0, 0);
+    const plataforma = map.createLayer("plataforma", tileset, 0, 0);
 
-  // Busca los objetos de los jugadores en la capa de objetos
-  const objetos = map.getObjectLayer('objetos').objects;
-  const player1Spawn = objetos.find(obj => obj.name === 'player1');
-  const player2Spawn = objetos.find(obj => obj.name === 'player2');
+    // primero cargar el score
 
-  // Crea las naves en las posiciones de los objetos
-  this.naveRoja = this.physics.add.sprite(player1Spawn.x, player1Spawn.y, 'naveRoja');
-  this.naveAzul = this.physics.add.sprite(player2Spawn.x, player2Spawn.y, 'naveAzul');
+    // segundo cargar el personaje
 
-  // Haz que las naves colisionen con las plataformas
-  this.physics.add.collider(this.naveRoja, plataformas);
-  this.physics.add.collider(this.naveAzul, plataformas);
+    // para disparar en direccion donde mira el jugador
+    // jugador x - y velocityXY
+    // crear un rectagulo que se mueva en la direccion del jugador
+
+    this.add.rectangle(300, 100, 80, 10, 0x00f000, 0.5);
+
+    // //plataforma.setCollisionByProperty({ collides: true }); // Si marcaste tiles como colisionables en Tiled
+
+    // // Busca los objetos de los jugadores en la capa de objetos
+    // const objetos = map.getObjectLayer("objetos").objects;
+    // const player1Spawn = objetos.find((obj) => obj.name === "player1");
+    // const player2Spawn = objetos.find((obj) => obj.name === "player2");
+
+    // // Crea las naves en las posiciones de los objetos
+    // this.naveRoja = this.physics.add.sprite(
+    //   player1Spawn.x,
+    //   player1Spawn.y,
+    //   "naveRoja"
+    // );
+    // this.naveAzul = this.physics.add.sprite(
+    //   player2Spawn.x,
+    //   player2Spawn.y,
+    //   "naveAzul"
+    // );
+
+    // // Haz que las naves colisionen con las plataformas
+    // this.physics.add.collider(this.naveRoja, plataformas);
+    // this.physics.add.collider(this.naveAzul, plataformas);
   }
-  }
+}
